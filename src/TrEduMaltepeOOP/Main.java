c2ce589e06cb219e2fae2b77272f5c156b85c770
Muharrem <190704041@st.maltepe.edu.tr>
2022-03-01 13:24:01 +0300

    class week1

---
+++ src/TrEduMaltepeOOP/Main.java
@@ -0,0 +1,10 @@
+package TrEduMaltepeOOP;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world :(:):");
+
+    }
+}
